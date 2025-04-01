fe04e5f1d3c0c1e83a51b0e48dcdf14af594d8e2
kimjunho <wnsghi1@naver.com>
2025-04-01 18:13:15 +0900

    feat : mysql - volumes 마운트 설정

--- repository/src/main/resources/data.sql
+++ repository/src/main/resources/data.sql
@@ -2,4 +2,6 @@ INSERT INTO users (name, email)
 VALUES
     ('Alice Smith', 'alice.smith@example.com'),
     ('Bob Johnson', 'bob.johnson@example.com'),
+    ('Ronaldo', 'c.ronaldo@example.com'),
+    ('Messi', 'r.messi@example.com'),
     ('Charlie Brown', 'charlie.brown@example.com');
